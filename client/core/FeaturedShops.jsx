21de9c22e8f1fcbada661cfabebc57113467d5cc
Samantha Shirley <hello@samanthashirley.com>
2023-11-26 19:06:00 -0500

    Add Latest Products and New Shops to Home

---
+++ client/core/FeaturedShops.jsx
@@ -0,0 +1,112 @@
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import { list } from "./../shop/api-shop.js";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: "100%",
+    margin: "auto",
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+    padding: theme.spacing(3),
+    boxShadow: "none",
+  },
+  title: {
+    margin: `${theme.spacing(3)}px 0 ${theme.spacing(2)}px`,
+    color: theme.palette.primary.main,
+    textAlign: "center",
+    fontSize: "2em",
+  },
+  shop: {
+    padding: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+    backgroundColor: theme.palette.grey[100], // Light background color
+    borderRadius: theme.spacing(2),
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+  },
+  link: {
+    textDecoration: "none",
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    marginBottom: theme.spacing(2),
+  },
+  shopDetails: {
+    flex: 1,
+  },
+  shopTitle: {
+    fontSize: "1.5em",
+    marginBottom: theme.spacing(1),
+  },
+  subheading: {
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function Shops(props) {
+  const classes = useStyles();
+  const [shops, setShops] = useState([]);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    const signal = abortController.signal;
+    list(signal).then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setShops(data);
+      }
+    });
+    return function cleanup() {
+      abortController.abort();
+    };
+  }, []);
+
+  // Limit the number of shops based on the 'limit' prop
+  const limitedShops = shops.slice(0, props.limit);
+
+  return (
+    <div>
+      <Paper className={classes.root} elevation={4}>
+        <Typography variant="h6" className={classes.title}>
+          Check Out the Newest Shops
+        </Typography>
+        <Grid container spacing={3}>
+          {limitedShops.map((shop, i) => (
+            <Grid item xs={12} sm={6} md={4} key={i}>
+              <Link to={"/shops/" + shop._id} className={classes.link}>
+                <Paper className={classes.shop}>
+                  <Avatar
+                    className={classes.avatar}
+                    src={
+                      "/api/shops/logo/" + shop._id + "?" + new Date().getTime()
+                    }
+                  />
+                  <div className={classes.shopDetails}>
+                    <Typography variant="h6" className={classes.shopTitle}>
+                      {shop.name}
+                    </Typography>
+                    <Typography
+                      variant="subtitle2"
+                      className={classes.subheading}
+                    >
+                      {shop.description}
+                    </Typography>
+                  </div>
+                </Paper>
+              </Link>
+            </Grid>
+          ))}
+        </Grid>
+      </Paper>
+    </div>
+  );
+}
--- client/core/Home.jsx
+++ client/core/Home.jsx
@@ -1,27 +1,3 @@
-/*import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
-import CardMedia from '@material-ui/core/CardMedia'
-import Typography from '@material-ui/core/Typography'
-import unicornbikeImg from './../assets/images/unicornbike.jpg'
-
-const useStyles = makeStyles(theme => ({ 
-card: {
-maxWidth: 600, 
-margin: 'auto',
-marginTop: theme.spacing(5) 
-},
-title: {
-padding:`${theme.spacing(3)}px ${theme.spacing(2.5)}px 
-${theme.spacing(2)}px`,
-color: theme.palette.openTitle 
-},
-media: { 
-minHeight: 400
-} 
-}))*/
-
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
@@ -29,10 +5,14 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import homesplashImg from "./../assets/images/home_splash.png";
+import { listLatest } from "./../product/api-product.js";
+import LatestProducts from "./../core/LatestProducts";
+import { list } from "./../shop/api-shop.js";
+import FeaturedShops from "./../core/FeaturedShops";
 
 const useStyles = makeStyles((theme) => ({
   card: {
-    maxWidth: 800,
+    maxWidth: "100%",
     margin: "auto",
     marginTop: theme.spacing(5),
   },
@@ -43,49 +23,76 @@ const useStyles = makeStyles((theme) => ({
   media: {
     minHeight: 600,
   },
+  body2: {
+    textAlign: "center",
+    margin: "auto",
+  },
 }));
 
 export default function Home() {
   const classes = useStyles();
-  return (
-    <Card className={classes.card}>
-      <Typography variant="h6" className={classes.title}>
-        Home Page
-      </Typography>
-      <CardMedia
-        className={classes.media}
-        image={homesplashImg}
-        title="8-Bit Bazaar"
-      />
-      <CardContent>
-        <Typography variant="body2" component="p">
-          Welcome to 8-Bit Bazaar Video Game Marketplace
-        </Typography>
-      </CardContent>
-    </Card>
-  );
-}
 
-/*const MyComponent = () => {
-  const classes = useStyles();
+  // Display product grid of latest products and random shops
+  const [data, setData] = React.useState({ products: [], shops: [] });
+  const [error, setError] = React.useState("");
+
+  React.useEffect(() => {
+    const abortController = new AbortController();
+    const signal = abortController.signal;
+
+    // Fetch latest products
+    listLatest(signal)
+      .then((productData) => {
+        if (productData.error) {
+          setError(productData.error);
+        } else {
+          // Fetch random shops
+          list(signal)
+            .then((shopData) => {
+              if (shopData.error) {
+                setError(shopData.error);
+              } else {
+                setData({ products: productData, shops: shopData });
+              }
+            })
+            .catch((error) => setError(error.message));
+        }
+      })
+      .catch((error) => setError(error.message));
+
+    return function cleanup() {
+      abortController.abort();
+    };
+  }, []);
 
   return (
-    <Card className={classes.card}>
-      <CardContent>
+    <div>
+      <Card className={classes.card}>
         <Typography variant="h6" className={classes.title}>
-          Card Title
+          {/* Welcome to 8-Bit Bazaar */}
         </Typography>
         <CardMedia
           className={classes.media}
-          image={unicornbikeImg}
-          title="Unicorn Bike"
+          image={homesplashImg}
+          title="8-Bit Bazaar "
         />
+        <CardContent>
+          <Typography variant="body2" component="p">
+            Welcome to the 8-Bit Bazaar home page.
+          </Typography>
+        </CardContent>
+      </Card>
+      <LatestProducts products={data.products} />
+      <FeaturedShops shops={data.shops} />
+      <Card className={classes.card}>
         <Typography variant="body2" component="p">
-          Card content goes here.
+          <center>
+            Copyright 2023 - 8-Bit Bazaar by Fusion Factory - Brayden Bouchard,
+            Marcus Charles, Vincent Chen, Lyndsay Riches, Sarah Shields and
+            Samantha Shirley
+          </center>
         </Typography>
-      </CardContent>
-    </Card>
+      </Card>
+    </div>
   );
-};
-
-export default MyComponent;*/
+}
